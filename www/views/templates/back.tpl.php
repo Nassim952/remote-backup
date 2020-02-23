7a53075b7edf14826aba2a72c0307faa5d49fb5a
nassim <nasfahdine@outlook.com>
2020-02-23 13:51:24 +0100

    cleaning sidebar minor fix

--- www/views/templates/back.tpl.php
+++ www/views/templates/back.tpl.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,7 +14,89 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.js"></script>
     <script src="../../js/chart.js"></script>
 </head>
+
 <body>
-    <?php include "views/".$this->view.".php" ?>
+    <div class="flex-container">
+        <div class="sidebar">
+            <div class="title-container">
+                <div>
+                    <h1>NEAR BY</h1>
+                </div>
+                <div id="subtitle-fixer">
+                    <h3 id="text-white" style="font-size:20px;">Dashboard</h3>
+                </div>
+            </div>
+            <div class="name-container">
+                <span id="dot"></span>
+                <p>John Doe</p>
+            </div>
+            <div class="nav-content">
+                <h2 id="text-submenu-fixer">Cinestar</h2>
+                <div class="dashboard-menu">
+                    <div class="sidebar-sub-headers">
+                        <div class="fas fa-film fa-lg"></div>
+                        <div id="submenu-wrapper">
+                            <a href="<?= helpers::getUrl("User", "dashboard") ?>" id="text-white"><span>Films</span></a>
+                        </div>
+                    </div>
+                </div>
+                <h2 id="text-submenu-fixer">Diffusion</h2>
+                <div class="dashboard-menu">
+                    <div class="sidebar-sub-headers">
+                        <div class="fas fa-clock fa-lg"></div>
+                        <div id="submenu-wrapper">
+                            <span>Horaires</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="dashboard-menu">
+                    <div class="sidebar-sub-headers">
+                        <div class="fas fa-sliders-h fa-lg"></div>
+                        <div id="submenu-wrapper">
+                            <span>Salles</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="dashboard-menu">
+                    <div class="sidebar-sub-headers">
+                        <div class="fas fa-video fa-lg"></div>
+                        <div id="submenu-wrapper">
+                            <span>Cinema</span>
+                        </div>
+                    </div>
+                </div>
+                <h2 id="text-submenu-fixer">KPI</h2>
+                <div class="dashboard-menu">
+                    <div class="sidebar-sub-headers">
+                        <div class="fas fa-users fa-lg"></div>
+                        <div id="submenu-wrapper">
+                            <a href="<?= Helpers::getUrl("User","stat") ?>" id="text-white"><span>Stats</span></a>
+                        </div>
+                    </div>
+                </div>
+                <h2 id="text-submenu-fixer">Gestion</h2>
+                <div class="dashboard-menu">
+                    <div class="sidebar-sub-headers">
+                        <div class="fas fa-users fa-lg"></div>
+                        <div id="submenu-wrapper">
+                            <span>Users</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="dashboard-menu">
+                    <div class="sidebar-sub-headers">
+                        <div class="fas fa-user-shield fa-lg"></div>
+                        <div id="submenu-wrapper">
+                            <span>Admin</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- INCLUDE VIEWS HERE -->
+        <?php include "views/".$this->view.".php" ?>
+        <!-- END INCLUDE VIEWS -->
+    </div>
 </body>
+
 </html>
