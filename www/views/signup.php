ec5cc6271de3980b5b5be8a7780cadf04528b583
Randy-Mb <randymb77@gmail.com>
2020-02-23 21:27:46 +0100

    signup + signin .php & css ajoutés

---
+++ www/views/signup.php
@@ -0,0 +1,51 @@
+<html>
+	<head>
+		<title>NEAR BY - Inscription</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="../../dist/style.css"/>
+		<link rel="stylesheet" href="../../dist/mediaqueries.css"/>
+	</head>
+
+	<body>
+		<main class="container">
+			<section class="section1">
+				<!-- IMAGE RED.PNG -->
+			</section>
+
+			<section class="section2">
+				<header class="headerSection2">
+					<div class="containerImg">
+						<img src="../../src/images/logo.png">
+					</div>
+				</header>
+
+				<section class="sectionSignup">
+					<h1>Créez votre compte !</h1>
+					<br>
+					<div class="divInput">
+						<input type="text" name="firstname" id="firstname" placeholder="Nom">
+						<input type="text" name="lastname" id="lastname" placeholder="Prenom">
+						<br>
+						<input type="email" name="email" id="email" placeholder="Email">
+						<br>
+						<input type="password" name="password" id="" placeholder="Mot de passe">
+						<br>
+						<input type="password" name="confirmpwd" id="" placeholder="Confirmer mot de passe">
+						<br>
+
+						<div class="divCheckbox">
+							<input type="checkbox">
+							<label>J'accepte les conditions générales</label>
+							<br>
+							<input type="checkbox">
+							<label>J'accepte de recevoir des offres spéciales par mail</label>
+						</div>
+						<br>
+						<center><button type="submit">S'inscrire</button></center>
+					</div>
+				</section>
+			</section>
+		</main>
+	</body>
+</html>
