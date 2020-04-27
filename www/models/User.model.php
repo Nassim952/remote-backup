6dfdb0663e4ff0e0e3a3eb29162cc06bd98fff87
Eyramds <eyram.souza@gmail.com>
2020-04-27 11:06:46 +0200

    User/hydrate

--- www/models/User.model.php
+++ www/models/User.model.php
@@ -1,5 +1,9 @@
 <?php 
 class user extends DB{
+
+    protected $id=null;
+    protected $firstname;
+    protected $lastname;
     protected $login;
     protected $password;
     protected $type;
@@ -8,11 +12,35 @@ class user extends DB{
         parent::__construct();
     }
 
+    public function hydrade($data){
+        array_walk($data, function($key, $value){
+            if(isset($data[$key])){
+                'set'.$key($value);
+            }   
+        });
+    }
+
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    public function setFirstname($firstname){
+        $this->firstname = trim($firstname);
+    }
+
+    public function setLastname($lastname){
+        $this->lastname = trim($lastname);
+    }
+    
     public function setLogin($login){
-        $this->login = $login;
+        $this->login = trim($login);
     }
 
     public function setPassword($password){
         $this->password = $password;
+    }
+
+    public function setType($type){
+        $this->type = $type;
     }
 }
