7b803a7253af5091baa271af753087ada09777a0
Eyramds <eyram.souza@gmail.com>
2020-06-19 14:30:46 +0200

    adding forms register/login

--- www/forms/LoginType.php
+++ www/forms/LoginType.php
@@ -6,11 +6,18 @@ use App\Core\Helper;
 
 class LoginType extends Form {
 
-    public function getForm(){
+    public function getForm()
+    {
         return $this->builder
             ->addField("firstname", "text", [
                 'placeholder' => 'toto'
             ])
-            ->addField;
+            ->addField( "pwd", "password", [
+                "placeholder"=>"Votre mot de passe",
+                "class"=>"form-control form-control-user",
+                "id"=>"",
+                "required"=>true,
+                "errorMsg"=>"Votre mot de passe doit faire entre 6 et 20 caractères avec une minuscule et une majuscule"
+            ]);
     }
 }
