6569680610ff3522331fe955ce0753093de042e5
Mmadi <nasfahdine@outlook.com>
2020-06-27 21:51:29 +0200

    minor fix

--- www/controllers/UserController.php
+++ www/controllers/UserController.php
@@ -131,33 +131,28 @@ class UserController extends Controller{
                 "form" => $registerType
             ]);
         }
-      
+    
 	}
 	
-    public function registerAction()
-    {
-        $registerType = new RegisterType();
+    // public function registerAction()
+    // {
+    //     $registerType = new RegisterType();
 
-        if ( $_SERVER["REQUEST_METHOD"] == "POST") {
-            //Vérification des champs
-            $this->render("register", "account", [
-                "form" => $registerType,
-                "errors" => Validator::formRegisterValidate( $registerType, $_POST )
-            ]);
-        } else {
-            $this->render("register", "account", [
-                "form" => $registerType
-            ]);
-        }
-    }
+    //     if ( $_SERVER["REQUEST_METHOD"] == "POST") {
+    //         //Vérification des champs
+    //         $this->render("register", "account", [
+    //             "form" => $registerType,
+    //             "errors" => Validator::formRegisterValidate( $registerType, $_POST )
+    //         ]);
+    //     } else {
+    //         $this->render("register", "account", [
+    //             "form" => $registerType
+    //         ]);
+    //     }
+    // }
     
     public function buildPage()
     {
 
     }
-
-
-	public function forgetPwdAction(){
-		$myView = new View("forgetPwd", "account");
-	}
 }
