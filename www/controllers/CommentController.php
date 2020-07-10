0054ba761f9cb2a3a6e2206dee32322a65d1093f
Andrew Mondor <andrewmondor@macbook-pro-de-andrew-1.home>
2020-07-10 05:06:09 +0200

    updating comment crud, read done

---
+++ www/controllers/CommentController.php
@@ -0,0 +1,38 @@
+<?php
+namespace cms\controllers;
+
+use cms\core\Controller;
+use cms\managers\CommentManager;
+use cms\models\Comment;
+
+class CommentController extends Controller{
+    private $comments;
+
+    
+    public function commentAction(){
+        $commentManager = new CommentManager(Comment::class, 'Comment');
+        $comments = $commentManager->read();
+        // This send comment data to the view thanks to the Commentmanager read function
+        $this->render("comment", "back", ['comments'=> $comments ]);
+
+    }
+
+    public function deleteCommentAction(){
+
+        $commentManager = new CommentManager(Comment::class, 'Comment');
+        $comments = $commentManager->read();
+        // This send comment data to the view thanks to the Commentmanager read function
+        $this->render("comment", "back", ['comments'=> $comments ]);
+
+        if ( $_SERVER["REQUEST_METHOD"] == "POST"){
+            $id = $_POST['id'];
+
+            $commentManager = new CommentManager(Comment::class, 'comment');
+            $commentManager->delete($id);
+            // refresh the page after delete
+            echo("<meta http-equiv='refresh' content='1'>");
+        }
+       
+    }
+   
+}
