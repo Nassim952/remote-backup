ef045d4a6f706f87bcf4ac312e6a62d50f0c0949
bilal <bilal.belatoui@gmail.com>
2020-07-11 14:07:45 +0200

    AddFilmType

--- www/forms/AddFilmType.php
+++ www/forms/AddFilmType.php
@@ -10,97 +10,119 @@ use cms\core\Helper;
 
 class AddFilmType extends Form {
     
-    public static function getForm()
+    public static function initForm()
     {
         return $this->builder
-                ->addField("title", "text", [
-                    "required"=>true,
-                    "placeholder"=>"Titre du film",
-                    "class"=>"input-form",
-                    "id"=>"title",
-                    "minlength"=>2,
-                    "maxlength"=>100,
-                    "errorMsg"=>"Votre titre doit faire entre 2 et 50 caractères"
-                    ])
-                ->addField("duration", "time", [
-                    "placeholder"=>"Durée du film",
-                    "class"=>"input-form",
-                    "id"=>"duration",
-                    "required"=>true,
-                    "errorMsg"=>"Votre date est au mauvais format"
-                ])
-                ->addField("genre", "select", [
-                    "placeholder"=>"Genre",
-                    "class"=>"input-form",
-                    "id"=>"genre",
-                    "required"=>true,
-                    "option"=>[
-                        "action"=>"Action", 
-                        "aventure"=>"Aventure", 
-                        "horreur"=>"Horreur", 
-                        "animation"=>"Animation", 
-                        "comedie"=>"Comedie"
-                    ], 
-                    "errorMsg"=>"error"
-                ])
-                ->addField("age", "select", [
-                    "placeholder"=>"Age",
-                    "class"=>"input-form",
-                    "id"=>"age",
-                    "required"=>true,
-                    "option"=>[
-                        "-10"=>"-10", 
-                        "-12"=>"-12", 
-                        "-16"=>"-16", 
-                        "-18"=>"-18"
-                    ], 
-                    "errorMsg"=>"error"
-                ])
-                ->addField("date", "date", [
-                    "placeholder"=>"Date de sortie",
-                    "class"=>"input-form",
-                    "id"=>"date",
-                    "required"=>true,
-                    "errorMsg"=>"Votre date n'est pas au format JJ/MM/AAAA"
-                ])
-                ->addField("real", "text", [
-                    "placeholder"=>"Réalisateur",
-                    "class"=>"input-form",
-                    "id"=>"real",
-                    "required"=>true,
-                    "min-length"=>1,
-                    "max-length"=>100,
-                    "errorMsg"=>"Le nom du réalisateur doit comporter entre 1 et 100 caractères"
-                ])
-                ->addField("actor", "text", [
-                    "placeholder"=>"Acteur principal",
-                    "class"=>"input-form",
-                    "id"=>"actor",
-                    "required"=>true,
-                    "min-length"=>1,
-                    "max-length"=>100,
-                    "errorMsg"=>"Le nom de l'acteur doit comporter entre 1 et 100 caractères"
-                ])
-                ->addField("nationality", "select", [
-                    "placeholder"=>"Nationalité",
-                    "class"=>"input-form",
-                    "id"=>"nationality",
-                    "required"=>true,
-                    "option"=>[
-                        "fr"=>"France", 
-                        "usa"=>"Etat-Unis", 
-                        "es"=>"Espagne"
+                ->add('title', 'text', [
+                    'attr'=>[
+                        'placeholder'=>'Titre du film',
+                        'id'=>'title',
+                        'class'=>'input-form'
                     ],
-                    "errorMsg"=>"error"
+                    'required'=>true,
+                    'constraints' => [
+                        new Length(2,100, 'Votre titre doit contenir au moins 2 caractères', 'Votre titre doit contenir au plus 50 caractères')
+                    ]
                 ])
-                ->addField("poster", "url", [
-                    "type"=>"url",
-                    "placeholder"=>"Affiche du film",
-                    "class"=>"input-form",
-                    "id"=>"poster",
-                    "required"=>true,
-                    "errorMsg"=>"error file"
+                ->add('duration', 'time', [
+                    'attr'=>[
+                        'placeholder'=>'Durée du film',
+                        'id'=>'duration',
+                        'class'=>'input-form'
+                    ],
+                    'required'=>true,
+                ])
+                ->add('genre', 'select', [
+                    'attr'=>[
+                        'placeholder'=>'Genre',
+                        'id'=>'genre',
+                        'class'=>'input-form'
+                    ],
+                    'required'=>true,
+                    'option'=>[
+                        'action'=>'Action', 
+                        'aventure'=>'Aventure', 
+                        'horreur'=>'Horreur', 
+                        'animation'=>'Animation', 
+                        'comedie'=>'Comedie'
+                    ],
+                ])
+                ->add('age', 'select', [
+                    'attr'=>[
+                        'placeholder'=>'Age',
+                        'id'=>'age',
+                        'class'=>'input-form'
+                    ],
+                    'required'=>true,
+                    'option'=>[
+                        '-10'=>'-10', 
+                        '-12'=>'-12', 
+                        '-16'=>'-16', 
+                        '-18'=>'-18'
+                    ],
+                ])
+                ->add('duration', 'time', [
+                    'attr'=>[
+                        'placeholder'=>'Durée du film',
+                        'id'=>'duration',
+                        'class'=>'input-form'
+                    ],
+                    'required'=>true,
+                ])
+                ->add('title', 'text', [
+                    'attr'=>[
+                        'placeholder'=>'Réalisateur',
+                        'id'=>'real',
+                        'class'=>'input-form'
+                    ],
+                    'required'=>true,
+                    'constraints' => [
+                        new Length(2,100, 'Le nom du réalisateur doit contenir au moins 2 caractères', 'Le nom du réalisateur doit contenir au plus 50 caractères')
+                    ]
+                ])
+                ->add('actor', 'text', [
+                    'attr'=>[
+                        'placeholder'=>'Acteur principal',
+                        'id'=>'actor',
+                        'class'=>'input-form'
+                    ],
+                    'required'=>true,
+                    'constraints' => [
+                        new Length(2,100, "Le nom de l'acteur doit contenir au moins 2 caractères", "Le nom de l'acteur doit contenir au plus 50 caractères")
+                    ]
+                ])
+                ->add('nationality', 'select', [
+                    'attr'=>[
+                        'placeholder'=>'Nationalité',
+                        'id'=>'nationality',
+                        'class'=>'input-form'
+                    ],
+                    'required'=>true,
+                    'option'=>[
+                        'fr'=>'France', 
+                        'usa'=>'Etat-Unis', 
+                        'es'=>'Espagne'
+                    ],
+                ])
+                ->add('poster', 'url', [
+                    'attr'=>[
+                        'placeholder'=>'Affiche du film',
+                        'id'=>'poster',
+                        'class'=>'input-form'
+                    ],
+                    'required'=>true,
                 ]);
         }
+
+        public function configureOptions(): void
+        {
+        $this
+            ->addConfig('class', Dashboard::class)
+            ->setName('addFilm')
+            ->addConfig('attr', [
+                "method" => "POST",
+                "id"=>"formAddFilm",
+                "class"=>"dashboard",
+            ]);
+        }
     }
-}
