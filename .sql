57126d72ff2e1db8d9c1cf504b655b83a926e46e
Andrew Mondor <andrewmondor@macbook-pro-de-andrew.home>
2020-07-21 16:54:08 +0200

    adding references in database

--- .sql
+++ .sql
@@ -127,33 +127,9 @@ CREATE TABLE IF NOT EXISTS bape_article_image(
     image_id INT NOT NULL,
     FOREIGN KEY (article_id) REFERENCES bape_article(id) ON DELETE CASCADE,
     FOREIGN KEY (image_id) REFERENCES bape_image(id) ON DELETE CASCADE
+
 );
 
---
--- Structure de la table `comment`
---
-
-DROP TABLE IF EXISTS bape_comment;
-CREATE TABLE IF NOT EXISTS bape_comment(
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    comment TEXT,
-    post_date TIMESTAMP,
-    user_id INT NOT NULL,
-    target INT NOT NULL,
-    target_type INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES bape_user(id) ON DELETE CASCADE
-);
-
-
--- 
--- Chargement des donnees de la table user
--- 
-
-INSERT INTO bape_comment (id, comment, post_date, user_id, target, target_type) VALUES
-(null,"Spiderman respecte parfaitement bien la BD. Les acteurs sont bien choisis et incarnent à merveille leurs rôles. Avec des personnages sont attachants en particulier Peter Parker qui bien qu'ayant de super pouvoirs reste dominer par son côté humain.","2020-07-16 14:40:34",5,3,3),
-(null,"Eh bien voilà, 17 ans d'attente, et le retour de nos Bad Boys favoris ! Ce qui marque avant tout c'est que l'alchimie entre Will Smith et Martin Lawrence n'a pas pris une seule ride. Leurs interactions sont toujours savoureuses.","2020-06-15 17:30:22",4,2,2),
-(null,"'Suicide Squad' de David Ayer, après quelques déceptions récentes, fait cette fois figure d'une bonne surprise, enfin très drôle et vue sous un angle nouveau et différent ! DC Comics nous prouve que l'humour a ici sa place et c'est là le gros point fort... Et oui, surtout ne pas se prendre au sérieux comme l'était le suffisant 'Batman v Superman'...","2019-02-20 16:40:34",3,1,1),
-(null,"Un excellent divertissement ! Le scénario, qui ressemble à celui de Point Break, tient bien la route. Les courses poursuites sont très bien filmées et le casting est parfait, la présence de Paul Walker et Vin Diesel apporte beaucoup a ce superbe film d'action.","2019-05-18 10:20:22",2,4,4);
 
 --
 -- Structure de la table `component_page`
@@ -200,6 +176,37 @@ INSERT INTO bape_movie (title, release_date, duration, kind, age_require, direct
 ('Suicide Squad', '2020-01-02', '02:03','Action', 16, 'David Ayer','Will Smith','American','Long-metrage','https://images-na.ssl-images-amazon.com/images/I/A1gFCETVqVL._AC_SL1500_.jpg', 'Cest tellement jouissif dêtre un salopard ! Face à une menace aussi enigmatique quinvincible, lagent secret Amanda Waller reunit une armada de crapules de la pire espece.'),
 ('Fast and Furious 8', '2018-02-01', '02:03', 'Action', 12, 'Gary Gray','Paul Walker','American','Long-metrage','https://fr.web.img3.acsta.net/pictures/17/03/03/17/27/305158.jpg', 'Maintenant que Dom et Letty sont en lune de miel, que Brian et Mia se sont ranges et que le reste de l’equipe a ete disculpe, la bande de globetrotteurs retrouve un semblant de vie normale.'),
 ('Je suis une legende', '2007-02-19', '01:40', 'Thriller', 18, 'Francis Lawrence','Will Smith','American','Long-metrage','https://fr.web.img3.acsta.net/medias/nmedia/18/63/18/01/18866172.jpg', 'Robert Neville etait un savant de haut niveau et de reputation mondiale, mais il en aurait fallu plus pour stopper les ravages de cet incurable et terrifiant virus dorigine humaine.');
+
+
+
+
+--
+-- Structure de la table `comment`
+--
+
+DROP TABLE IF EXISTS bape_comment;
+CREATE TABLE IF NOT EXISTS bape_comment(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    comment TEXT,
+    post_date TIMESTAMP,
+    user_id INT NOT NULL,
+    target INT NOT NULL,
+    target_type INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES bape_user(id) ON DELETE CASCADE,
+    FOREIGN KEY (target) REFERENCES bape_movie(id) ON DELETE CASCADE
+);
+
+
+-- 
+-- Chargement des donnees de la table user
+-- 
+
+INSERT INTO bape_comment (id, comment, post_date, user_id, target, target_type) VALUES
+(null,"Spiderman respecte parfaitement bien la BD. Les acteurs sont bien choisis et incarnent à merveille leurs rôles. Avec des personnages sont attachants en particulier Peter Parker qui bien qu'ayant de super pouvoirs reste dominer par son côté humain.","2020-07-16 14:40:34",5,3,3),
+(null,"Eh bien voilà, 17 ans d'attente, et le retour de nos Bad Boys favoris ! Ce qui marque avant tout c'est que l'alchimie entre Will Smith et Martin Lawrence n'a pas pris une seule ride. Leurs interactions sont toujours savoureuses.","2020-06-15 17:30:22",4,2,2),
+(null,"'Suicide Squad' de David Ayer, après quelques déceptions récentes, fait cette fois figure d'une bonne surprise, enfin très drôle et vue sous un angle nouveau et différent ! DC Comics nous prouve que l'humour a ici sa place et c'est là le gros point fort... Et oui, surtout ne pas se prendre au sérieux comme l'était le suffisant 'Batman v Superman'...","2019-02-20 16:40:34",3,1,1),
+(null,"Un excellent divertissement ! Le scénario, qui ressemble à celui de Point Break, tient bien la route. Les courses poursuites sont très bien filmées et le casting est parfait, la présence de Paul Walker et Vin Diesel apporte beaucoup a ce superbe film d'action.","2019-05-18 10:20:22",2,4,4);
+
 
 --
 -- Structure de la table `room`
