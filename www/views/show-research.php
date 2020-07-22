1f3f151caff3d163d30987e4341ad6619300109b
Randy Mb <randymb77@gmail.com>
2020-07-22 16:06:30 +0200

    Bouton Retour vers le haut + bouton Voir plus (/show-research) fonctionnel

--- www/views/show-research.php
+++ www/views/show-research.php
@@ -5,9 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../../css/show-research.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   </head>
 
   <body>
+    <script>
+      jQuery(document).ready(function() {
+        var duration = 300;
+        jQuery(window).scroll(function() {
+          if (jQuery(this).scrollTop() > 300) {
+            // Défillement de 300 pixels ou plus
+            // Ajoute le bouton
+            jQuery('.top').fadeIn(duration);
+          } else {
+            // Enlève le bouton
+            jQuery('.top').fadeOut(duration);
+          }
+        });
+              
+        jQuery('.top').click(function(event) {
+          // Retour en haut animé au clic
+          event.preventDefault();
+          jQuery('html, body').animate({scrollTop: 0}, duration);
+          return false;
+        })
+      });
+    </script>
+
     <table class="tableMovie">
       <?php foreach($searched as $movie): ?>
         <tr class="trMovie">
@@ -23,10 +47,12 @@
             âge <?= $movie->getAge_require() ?>+
             </p>
 
-            <span><a href="/show-movie (LE-FILM-EN-QUESTION)" style="font-weight: bold;" class="hover">Voir plus</a></span>
+            <span><a href="/show-movie/<?= $movie->getId() ?>" style="font-weight: bold;" class="hover">Voir plus</a></span>
           </td>
         </tr>
       <?php endforeach; ?>
     </table>
+    
+    <a class="top" title="Go to top"> Remonter</a>
   </body>
 </html>
